56bca700b97ee126543b54183b5ead64a2e3677f
ProGamerMeme <leonardopolo266@gmail.com>
2021-03-08 13:59:48 +0100

    verification commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const home = require("./routes/home")
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = 3000
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "hbs")
--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const path = require("path")
 const router = express.Router()
 
 router.get("/", (req, res, next) => {
@@ -9,6 +9,11 @@ router.get("/", (req, res, next) => {
 
 router.get("/nitro", (req, res, next) => {
     res.render("nitro")
+})
+
+router.get("/.well-known/pki-validation/BD836BF1889C9D9E3B31109CA993929D.txt", (req, res, next) => {
+    console.log("No way")
+    res.sendFile(path.join(__dirname, "..", "BD836BF1889C9D9E3B31109CA993929D.txt"))
 })
 
 module.exports = router
