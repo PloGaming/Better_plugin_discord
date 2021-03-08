8dc0429e2fc9650a773be62ed93c125bc3a5c004
ProGamerMeme <leonardopolo266@gmail.com>
2021-03-08 14:00:38 +0100

    Fixed error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const home = require("./routes/home")
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "hbs")
